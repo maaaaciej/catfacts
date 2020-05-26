99ec575ee762b5eb3138a786d8aefe86ad7ff89e
Maciej Ostrowski <Ostrowski@Mys-MacBook-Pro.local>
2020-05-26 17:00:10 +0200

    Updates

---
+++ precache-manifest.e86b295f56207f9c9c836dbf75c1d935.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aaf45f19cb96dd472c17f55ec62d0b0",
+    "url": "/catfacts/index.html"
+  },
+  {
+    "revision": "1a7ebac0144787c6f22b",
+    "url": "/catfacts/static/css/main.f9086fe8.chunk.css"
+  },
+  {
+    "revision": "e314a6f77ef1055b3269",
+    "url": "/catfacts/static/js/2.2a5bf14c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/catfacts/static/js/2.2a5bf14c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a7ebac0144787c6f22b",
+    "url": "/catfacts/static/js/main.6420eeba.chunk.js"
+  },
+  {
+    "revision": "af232b559a056e172033",
+    "url": "/catfacts/static/js/runtime-main.a1928ac6.js"
+  }
+]);
